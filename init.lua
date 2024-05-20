0589ae12ef0a3b2a24be3f78c8821b1c1e6e0f67
aiden <aiden.corrigan@gmail.com>
2024-05-20 02:48:27 -0400

    Added templ support but not working

--- init.lua
+++ init.lua
@@ -566,11 +566,12 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        templ = {},
         gopls = {},
         pyright = {},
         prettier = {},
         eslint = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
